67164a4f33a006e38eb099038cf65d803ccfd359
Naffy Dharni <naffydharni006@gmail.com>
2022-09-14 18:26:47 +0530

    Extract Head to separate component

---
+++ website/components/Meta.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head"
+
+interface MetaProps {
+	title?: string
+	description?: string
+}
+
+export function Meta({title, description}: MetaProps) {
+	return (
+		<>
+			<Head>
+				<title>{title ? title : 'Naffy Dharni'}</title>
+				<meta
+					name='description'
+					content={description ? description : 'Front-End Developer'}
+				/>
+				<link
+					rel='icon'
+					href='/logo.png'
+				/>
+			</Head>
+		</>
+	)
+}
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,22 +1,12 @@
 import type { NextPage } from "next"
-import Head from "next/head"
 import { About, Footer, Hero, Navbar } from "../components"
+import { Meta } from "../components/Meta"
 import { Projects } from "../components/Projects"
 
 const Home: NextPage = () => {
 	return (
 		<div className='bg-base-200'>
-			<Head>
-				<title>Naffy Dharni</title>
-				<meta
-					name='description'
-					content='Front-End Developer'
-				/>
-				<link
-					rel='icon'
-					href='/logo.png'
-				/>
-			</Head>
+			<Meta />
 			<Navbar />
 			<main className='container px-10 mx-auto md:px-20'>
 				<Hero />
