9aa199b351af9f032eb4b8cdb1be0289c3039e6f
Naffy Dharni <naffydharni006@gmail.com>
2022-09-14 17:24:20 +0530

    New Icon Component

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { GithubIcon } from "./Icons"
 
 export function Navbar() {
 	return (
@@ -48,35 +49,24 @@ export function Navbar() {
 				</div>
 				<div className='navbar-center hidden lg:flex'>
 					<a
-						className='btn btn-ghost hover:bg-transparent hover:text-primary'
+						className='btn btn-ghost hover:bg-transparent hover:text-primary transition-colors duration-500'
 						href='#about'
 					>
 						About
 					</a>
 					<a
-						className='btn btn-ghost hover:bg-transparent hover:text-primary'
+						className='btn btn-ghost hover:bg-transparent hover:text-primary transition-colors duration-500'
 						href='#projects'
 					>
 						Projects
 					</a>
-					<a className='btn btn-ghost hover:bg-transparent hover:text-primary'>
+					<a className='btn btn-ghost hover:bg-transparent hover:text-primary transition-colors duration-500'>
 						Tech Stack
 					</a>
 				</div>
 				<div className='navbar-end'>
-					<a href="https://github.com/naffydharni006/naffydharni006" className='btn btn-ghost hover:bg-transparent'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='currentColor'
-							strokeWidth='2'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							className='h-5 w-5 hover:stroke-primary'
-						>
-							<path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
-						</svg>
+					<a href="https://github.com/naffydharni006/naffydharni006" className='btn btn-ghost hover:bg-transparent group'>
+						<GithubIcon className="h-5 w-5 group-hover:stroke-primary transition-colors duration-500" />
 					</a>
 				</div>
 			</nav>
