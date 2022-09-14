75e65c37bb3311222445f9d95e8961c6c7279b10
Naffy Dharni <naffydharni006@gmail.com>
2022-09-14 13:36:59 +0530

    Update links and fix react/no-unknown-property

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export function Navbar() {
 	return (
 		<>
@@ -37,9 +39,12 @@ export function Navbar() {
 							</li>
 						</ul>
 					</div>
-					<a className='btn btn-ghost normal-case text-xl hover:bg-transparent'>
-						<span className='text-primary mr-1'>Naffy</span> Dharni
-					</a>
+					<Link href='/'>
+						<a className='btn btn-ghost normal-case text-xl hover:bg-transparent'>
+							<span className='text-primary mr-1'>Naffy</span>{" "}
+							Dharni
+						</a>
+					</Link>
 				</div>
 				<div className='navbar-center hidden lg:flex'>
 					<a
@@ -48,7 +53,10 @@ export function Navbar() {
 					>
 						About
 					</a>
-					<a className='btn btn-ghost hover:bg-transparent hover:text-primary'>
+					<a
+						className='btn btn-ghost hover:bg-transparent hover:text-primary'
+						href='#projects'
+					>
 						Projects
 					</a>
 					<a className='btn btn-ghost hover:bg-transparent hover:text-primary'>
@@ -62,9 +70,9 @@ export function Navbar() {
 							viewBox='0 0 24 24'
 							fill='none'
 							stroke='currentColor'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 							className='h-5 w-5 hover:stroke-primary'
 						>
 							<path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
