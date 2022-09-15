892c54d72ccec42d3e3c9af15255b8683b2348b7
Naffy Dharni <naffydharni006@gmail.com>
2022-09-15 14:58:28 +0530

    Remove link from all projects btn

--- website/components/Projects.tsx
+++ website/components/Projects.tsx
@@ -28,7 +28,7 @@ export function Projects() {
             demoUrl='https://naffydharni.tk'
             repo='naffydharni006/naffydharni006/blob/main/website'
           />
-          <a href='' className='btn btn-primary col-span-1 self-center'>
+          <a className='btn btn-primary col-span-1 self-center'>
             All Projects
           </a>
         </div>
